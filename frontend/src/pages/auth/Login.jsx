2d9e827f7a72d3004742bf43f4c8aa014133e94a
anshRS <anshraj0426@gmail.com>
2023-09-08 00:20:32 +0530

    Created the Authentication UI Layout

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,57 +1,49 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
     return (
-        <header class="flex items-center">
-            <div class="h-[100vh] w-2/3 bg-violet-100"></div>
-            <div class="h-[100vh] w-1/3 bg-violet-200"></div>
-            <form class="z-10 absolute right-28 w-1/3 bg-white rounded-2xl drop-shadow-2xl flex flex-col p-12 font-medium">
-                <h1 class="pb-6 text-[20px] font-semibold">
-                    Welcome to 👋ChatApp
-                </h1>
-                <div class="flex">
-                    <div class="h-20 w-[22 mr-6">
-                        <h1 class="pb-2">Please sign in to your account and start the adventure</h1>
-                    </div>
+        <form class="h-[550px] min-w-[400px] lg:w-1/3 m-10 md:m-14 bg-white rounded-2xl drop-shadow-2xl flex flex-col  p-10 md:p-14 font-normal justify-center">
+            <h1 class="pb-2 text-[20px] font-semibold">
+                Welcome to ChatApp! 👋
+            </h1>
+            <div class="flex">
+                <div class="mr-6 text-[15px]">
+                    <h1 class="pb-2">Please sign in to your account and start the adventure</h1>
                 </div>
-                <div class="flex pt-4">
-                    <div class="h-20 w-full mr-6 relative">
-                        <h1 class="pb-2">Email or Username</h1>
-                        <input type="text"
-                            class="shadow-inner p-2 border-gray-300 border rounded-md w-full focus:outline-none focus:border-gray-400" />
-                    </div>
-                </div>
-                <div class="flex pt-4">
-                    <div class="h-20 w-full mr-6 relative">
-                        <h1 class="pb-2 flex items-center justify-between">Password</h1>
-                        <div class="relative">
-                            <input type="password"
-                                class="shadow-inner p-2 pr-10 border-gray-300 border rounded-md w-full focus:outline-none focus:border-gray-400" />
-                        </div>
-                    </div>
-                </div>
-                <div class="flex items-center pt-2">
-                    <input type="checkbox" id="remember_me"
-                        class="h-5 w-5 text-indigo-500 border-gray-300 rounded focus:ring-indigo-500" />
-                    <label for="remember_me" class="ml-2 text-gray-600">Remember Me</label>
-                </div>
-                <button class="bg-indigo-500 hover:bg-indigo-700 mt-8 rounded-md text-white px-5 py-2">
-                    Login
-                </button>
-                <p class="mx-auto mt-2">
-                    New on our platform?<a href="" class="text-indigo-500 hover:text-indigo-900 ml-1">
-                        Create an account</a>
-                </p>
-            </form>
-            <div class="z-10 absolute left-20 top-28 text-5xl text-indigo-900">
-                <h1 class="mb-4 font-bold">Transforming Group</h1>
-                <h1 class="mb-4 font-extrabold">Communication Into</h1>
-                <h1 class="mb-4 font-bold">Collaborative Experience</h1>
             </div>
-            <div class="z-10 absolute left-20 bottom-36 text-5xl text-indigo-900">
-                👋ChatApp
+
+            <div class="h-20mr-6 relative pt-4">
+                <h1 class="pb-2 font-medium text-slate-600">Email</h1>
+                <input type="text"
+                    class="shadow-inner p-2 text-slate-500 border-gray-300 border rounded-md w-full focus:outline-none focus:border-gray-400"
+                    placeholder="Enter your email"
+                />
             </div>
-        </header>
+
+            <div class="h-20 w-full mr-6 relative pt-4">
+                <h1 class="pb-2 flex items-center justify-between font-medium text-slate-600">Password</h1>
+                <div class="relative">
+                    <input type="password"
+                        class="shadow-inner text-slate-500 p-2 pr-10 border-gray-300 border rounded-md w-full focus:outline-none focus:border-gray-400"
+                        placeholder="Enter your password"
+                    />
+                </div>
+            </div>
+
+            <div class="flex items-center pt-5">
+                <input type="checkbox" id="remember_me"
+                    class="h-5 w-5 text-indigo-500 border-gray-300 rounded focus:ring-indigo-500" />
+                <label for="remember_me" class="ml-2 text-gray-600">Remember Me</label>
+            </div>
+            <button class="bg-indigo-500 hover:bg-indigo-700 mt-8 rounded-md text-white px-5 py-2">
+                Login
+            </button>
+            <p class="mx-auto mt-2 text-slate-600">
+                New on our platform?<Link to="/auth/register" class="text-indigo-500 hover:text-indigo-900 ml-1">
+                    Create an account</Link>
+            </p>
+        </form>
     )
 }
 
--- frontend/src/pages/auth/Register.jsx
+++ frontend/src/pages/auth/Register.jsx
@@ -1,79 +1,64 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
-    <div class="flex items-center">
-      <div class="h-[100vh] w-2/3 bg-violet-100"></div>
-      <div class="h-[100vh] w-1/3 bg-violet-200"></div>
-      <form class="z-10 absolute right-28 w-1/3 bg-white rounded-2xl drop-shadow-2xl flex flex-col p-12 font-medium">
-        <h1 class="pb-6 text-[20px] mx-auto font-semibold">
-          REGISTRATION FORM
-        </h1>
-        <div class="flex">
-          <div class="h-20 w-[22 mr-6">
-            <h1 class="pb-2">First Name</h1>
-            <input
-              type="text"
-              class="shadow-inner p-2 border-gray-300 border rounded-md w-full focus:outline-none focus:border-gray-400"
-            />
-          </div>
-          <div class="h-20 w-[220px]">
-            <h1 class="pb-2">Last Name</h1>
-            <input
-              type="text"
-              class="shadow-inner p-2 border-gray-300 border rounded-md w-full focus:outline-none focus:border-gray-400"
-            />
-          </div>
-        </div>
-        <div class="pt-4">
-          <h1 class="pb-2">Username</h1>
+    <form class="h-[550px] min-w-[400px] m-10 md:m-14 lg:w-1/3 bg-white rounded-2xl drop-shadow-2xl flex flex-col p-10 md:p-14 font-normal  justify-center">
+      <h1 class="pb-6 text-[20px] mx-auto font-semibold">
+        REGISTRATION FORM
+      </h1>
+      <div class="flex">
+        <div class="h-20 mr-6 w-full">
+          <h1 class="pb-2 font-medium text-slate-600">First Name</h1>
           <input
             type="text"
-            class="shadow-inner p-2 border-gray-300 border rounded-md w-full focus:outline-none focus:border-gray-400"
+            class="shadow-inner p-2 text-slate-500 border-gray-300 border w-full rounded-md focus:outline-none focus:border-gray-400"
+            placeholder="Enter your first name"
           />
         </div>
-        <div class="pt-4">
-          <h1 class="pb-2">Email</h1>
+        <div class="h-20 w-full">
+          <h1 class="pb-2 font-medium text-slate-600">Last Name</h1>
           <input
-            type="email"
-            class="shadow-inner p-2 border-gray-300 border rounded-md w-full focus:outline-none focus:border-gray-400"
+            type="text"
+            class="shadow-inner p-2 text-slate-500 border-gray-300 border w-full rounded-md focus:outline-none focus:border-gray-400"
+            placeholder="Enter your last name"
           />
         </div>
-        <div class="flex pt-4">
-          <div class="h-20 w-[220px] mr-6">
-            <h1 class="pb-2">Password</h1>
-            <input
-              type="password"
-              class="shadow-inner p-2 border-gray-300 border rounded-md w-full focus:outline-none focus:border-gray-400"
-            />
-          </div>
-          <div class="h-20 w-[220px]">
-            <h1 class="pb-2">Confirm Password</h1>
-            <input
-              type="password"
-              class="shadow-inner p-2 border-gray-300 border rounded-md w-full focus:outline-none focus:border-gray-400"
-            />
-          </div>
+      </div>
+      <div class="pt-4">
+        <h1 class="pb-2 font-medium text-slate-600">Email</h1>
+        <input
+          type="email"
+          class="shadow-inner p-2 text-slate-500 border-gray-300 border rounded-md w-full focus:outline-none focus:border-gray-400"
+          placeholder="Enter your email"
+        />
+      </div>
+      <div class="flex pt-4">
+        <div class="w-full h-20 mr-6">
+          <h1 class="pb-2 font-medium text-slate-600">Password</h1>
+          <input
+            type="password"
+            class="shadow-inner p-2 text-slate-500 border-gray-300 border rounded-md w-full focus:outline-none focus:border-gray-400"
+          />
         </div>
-        <button class="bg-indigo-500 hover:bg-indigo-700 mt-8 rounded-md text-white px-5 py-2">
-          Register
-        </button>
-        <p class="mx-auto mt-2">
-          Already have an account?
-          <a href="" class="text-indigo-500 hover:text-indigo-900 ml-1">
-            Login
-          </a>
-        </p>
-      </form>
-      <div class="z-10 absolute left-20 top-28 text-6xl text-indigo-900">
-        <h1 class="mb-4 font-bold">Transforming Group</h1>
-        <h1 class="mb-4 font-extrabold">Communication Into</h1>
-        <h1 class="mb-4 font-bold">Collaborative Experience</h1>
+        <div class="h-20 w-full">
+          <h1 class="pb-2 font-medium text-slate-600">Confirm Password</h1>
+          <input
+            type="password"
+            class="shadow-inner p-2 text-slate-500 border-gray-300 border rounded-md w-full focus:outline-none focus:border-gray-400"
+          />
+        </div>
       </div>
-      <div class="z-10 absolute left-20 bottom-36 text-5xl text-indigo-900">
-        👋ChatApp
-      </div>
-    </div>
+      <button class="bg-indigo-500 hover:bg-indigo-700 mt-8 rounded-md text-white px-5 py-2">
+        Register
+      </button>
+      <p class="mx-auto mt-2 text-slate-600">
+        Already have an account?
+        <Link to="/auth/login" class="text-indigo-500 hover:text-indigo-900 ml-1">
+          Login
+        </Link>
+      </p>
+    </form>
   );
 };
 
