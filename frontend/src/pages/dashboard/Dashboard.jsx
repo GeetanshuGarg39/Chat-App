d819c223dc0a499110cd42d8c3ac4dee7846dec2
anshRS <anshraj0426@gmail.com>
2023-09-09 20:34:33 +0530

    Designed Chats & Groups section

---
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Chats from './Chats'
+
+const Dashboard = () => {
+    return (
+        <>
+            <Chats />
+        </>
+    )
+}
+
+export default Dashboard
