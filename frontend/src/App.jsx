e5eb1bed90d64f12d5a906298ca359f90af2b4dd
anshRS <anshraj0426@gmail.com>
2023-10-10 17:34:07 +0530

    Managed Snackbar component rendering

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
+import { useSelector } from 'react-redux'
 import './App.css'
 import Router from './routes'
+import Snackbar from './components/Snackbar';
 
 function App() {
 
+  const { open, message, severity } = useSelector((state) => state.app.snackbar);
+
   return (
     <>
-      <Router/>
+      <Router />
+      
+      {message && open ? <Snackbar
+        message={message}
+        open={open}
+        severity={severity}
+      /> : <></>}
+
     </>
   )
 }
