770b6a43204a54ab11ff6ab91b13c70548d5e733
Geetanshu Garg <geetanshugarg39@gmail.com>
2023-11-14 21:52:57 +0530

    Integerated get all users api

--- frontend/src/pages/dashboard/Chats.jsx
+++ frontend/src/pages/dashboard/Chats.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ChatBox from '../../components/ChatBox';
-import { ChatList } from '../../data';
+import axiosInst from '../../utils/axios';
+import { useSelector } from 'react-redux';
+
 
 const Chats = () => {
+    const { token } = useSelector((state) => state.auth);
+
+    const [chats,setChats] = useState([]);
+   
+    useEffect(() => {
+        const getAllChats = async () => {
+            const res = await axiosInst.get("/chats/users",{headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }})
+            console.log(chats);
+            setChats(res.data);
+        }
+        getAllChats();
+    },[chats])
+
     return (
         <div className="grow lg:shrink-0 scrollbar-hide overflow-y-auto lg:max-w-xs bg-slate-100 drop-shadow-[0_0px_0px_rgba(0,0,0,0.25)]">
             <div className="flex flex-col gap-3 p-6 bg-slate-100 md:bg-slate-100 sticky top-0 z-10 border-b">
@@ -25,7 +43,7 @@ const Chats = () => {
                     <span className='uppercase text-sm font-medium ml-3'>Pinned</span>
                 </p>
                 <div>
-                    {ChatList.filter((person) => person.pinned).map((person, i) => {
+                    {chats.filter((person) => person.pinned).map((person, i) => {
                         return <ChatBox key={i} {...person} />
                     })}
                 </div>
@@ -36,7 +54,7 @@ const Chats = () => {
                     <span className="uppercase text-sm font-medium ml-3">All Chats</span>
                 </p>
                 <div>
-                    {ChatList.filter((person) => !person.pinned).map((person, i) => {
+                    {chats.filter((person) => !person.pinned).map((person, i) => {
                         return <ChatBox key={i} {...person} />
                     })}
                 </div>
